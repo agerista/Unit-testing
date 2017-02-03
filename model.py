f0eaacce5576d507effa4285082ffb0415486982
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-03 12:45:46 -0800

    Unit and Flask testing

---
+++ model.py
@@ -0,0 +1,35 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    #FIXME: write a function that creates a game and adds it to the database.
+    funGame = Game(name='sparkle pants', description='trying on sparkly pants and stealing them from neighbors')
+    funGame2 = Game(name='snowflake sunshine', description='making snowflakes under sunshine without them melting')
+
+    db.session.add_all([funGame, funGame2])
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
+    print "Connected to DB."
